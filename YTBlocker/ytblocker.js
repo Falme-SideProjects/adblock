58560ca7046d197c6eca64be3eee91d7ed352535
Falme Streamless <falme_streamless@hotmail.com>
2021-02-16 03:28:22 -0300

    reduced code

--- YTBlocker/ytblocker.js
+++ YTBlocker/ytblocker.js
@@ -1,16 +1,11 @@
+const regexVal = /ads|_ad|ad_/g;
+
 function cancel(requestDetails) {
-    let respo = false;
-    let regex = /ads|_ad|ad_/g;
-    if(requestDetails.url.match(regex))
-    {
-        console.log("blocked address: "+requestDetails.url);
-        respo = true;
-    }
-    return {cancel: respo};
-  }
+    return {cancel: (requestDetails.url.match(regexVal))};
+}
   
-  browser.webRequest.onBeforeRequest.addListener(
+browser.webRequest.onBeforeRequest.addListener(
     cancel,
     {urls: ["<all_urls>"]},
     ["blocking"]
-  );
+);
